25086da7a16935b9babc9b473993aae70e2c9588
Sysindators <115671161+sysindators@users.noreply.github.com>
2022-10-31 20:42:08 +0700

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -2,22 +2,22 @@
 
 echo -e "\n\e[1;32m[!] In process of installation...\e[0m\n"
 
-sudo pip2 install requests
+sudo pip install requests
 
-sudo pip2 install rich
+sudo pip install rich
 
-sudo pip2 install phonenumbers
+sudo pip install phonenumbers
 
-sudo pip2 install opencage
+sudo pip install opencage
 
-sudo pip2 install folium
+sudo pip install folium
 
-sudo pip2 install geocoder
+sudo pip install geocoder
 
-sudo pip2 install bs4
+sudo pip install bs4
 
-sudo pip2 install scapy
+sudo pip install scapy
 
-sudo python -m pip2 install clear_cache
+sudo python -m pip install clear_cache
 
 echo -e "\n\e[1;32m[!] The installation is complete and run the command: python sysindators.py\e[0m"
