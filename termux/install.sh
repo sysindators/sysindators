39a6360ca3f9139f53b2dfde49f0656204fc5f65
Sysindators <115671161+sysindators@users.noreply.github.com>
2022-11-01 16:05:18 +0700

    Delete termux directory

--- termux/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "\n\e[1;32m[!] In process of installation...\e[0m\n"
-
-pkg install python -y
-
-python -m pip install --upgrade pip
-
-echo -e "\n\e[1;32m[!] The installation is complete and run the command: python sysindators.py\e[0m"
